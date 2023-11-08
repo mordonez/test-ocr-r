7bd730ea63d14ce8c3af998b56db61abb06ac9be
Miguel <mordonez.sanchez@gmail.com>
2023-11-08 16:17:31 +0100

    Update ocr.r

--- ocr.r
+++ ocr.r
@@ -5,24 +5,35 @@ library(tesseract)
 library(magick)
 
 # Lee la imagen
-image <- image_read("/Users/arianmartinez/Library/Mobile Documents")
+# URL de la imagen
+url <- "https://www.dropbox.com/scl/fi/ytbh22xy456g4zftiimqq/image_000139.tiff?rlkey=ez323gfqxxr114bsc5i32o9ie&raw=1"
+
+# Ruta donde se guardará la imagen
+destfile <- "image_000139.tiff"
+
+# Descarga la imagen
+download.file(url, destfile, mode = "wb")
+
+# Lee la imagen
+image <- image_read(destfile)
 
 # Realiza el OCR en la imagen
 text <- image %>% 
-  image_convert(colorspace = "gray") %>% 
+  image_convert(colorspace = "gray") %>%
   image_trim() %>% 
   image_ocr()
 
+print(paste(text))
 # Define una función para extraer la fecha
 extract_date <- function(text) {
-  date_pattern <- "\\b\\d{4}-\\d{2}-\\d{2}\\b"  # Ajusta este patrón si la fecha está en un formato diferente
+  date_pattern <- "\\b\\d{2}/\\d{2}/\\d{2}\\b"  # Patrón para fechas en el formato DD/MM/YY
   date <- regmatches(text, gregexpr(date_pattern, text))[[1]]
   return(date[1])
 }
 
 # Define una función para extraer la hora
 extract_time <- function(text) {
-  time_pattern <- "\\b\\d{2}:\\d{2}:\\d{2}\\b"  # Ajusta este patrón si la hora está en un formato diferente
+  time_pattern <- "\\b\\d{2}:\\d{2}:\\d{2}\\b"  # Patrón para horas en el formato HH:MM:SS
   time <- regmatches(text, gregexpr(time_pattern, text))[[1]]
   return(time[1])
 }
