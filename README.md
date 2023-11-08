fda898812d35df97afe40f053ad8a2257513975f
Miguel <mordonez.sanchez@gmail.com>
2023-11-08 19:54:05 +0000

    Add image processing and OCR functionality
    This commit includes the following changes:
    
    - Added a function to process images and extract date and time using OCR.
    - Modified the main script to iterate over image files in the 'frames' directory instead of reading from a CSV file.
    - Updated the README to reflect the changes in the script and usage instructions."

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# test-ocr-r
+# Proyecto de OCR con R
+
+Este proyecto utiliza OCR (Reconocimiento Óptico de Caracteres) para extraer la fecha y la hora de las imágenes y las guarda en un archivo CSV.
+
+## Dependencias
+
+Este proyecto depende de las siguientes bibliotecas de R:
+
+- magrittr
+- magick
+- tesseract
+
+## Uso
+
+1. Asegúrate de tener instaladas todas las dependencias.
+2. Ejecuta el script `main.R` para procesar las imágenes y extraer la fecha y la hora.
+
+## Funciones principales
+
+- `process_image_and_extract_datetime(image_path)`: Esta función toma la ruta a una imagen, procesa la imagen, realiza el OCR y extrae la fecha y la hora.
+
+## Datos de salida
+
+El script principal guarda los resultados en un archivo CSV llamado "updated_data.csv". Cada fila del archivo CSV corresponde a una imagen y contiene la fecha y la hora extraídas de la imagen.
--- ocr.r
+++ ocr.r
@@ -4,49 +4,91 @@ if (!require("magick")) install.packages("magick")
 library(tesseract)
 library(magick)
 
-# Lee la imagen
-# URL de la imagen
-url <- "https://www.dropbox.com/scl/fi/ytbh22xy456g4zftiimqq/image_000139.tiff?rlkey=ez323gfqxxr114bsc5i32o9ie&raw=1"
-
-# Ruta donde se guardará la imagen
-destfile <- "image_000139.tiff"
-
-# Descarga la imagen
-#download.file(url, destfile, mode = "wb")
-
-# Lee la imagen
-#image <- image_read(destfile)
-image <- image_read("image_000139.tiff")
-
-# Realiza el OCR en la imagen
-text <- image %>% 
-  image_resize("200%") %>%  # Escala la imagen
-  image_convert(colorspace = "gray") %>%  # Convierte a blanco y negro
-  image_modulate(brightness = 130, saturation = 0, hue = 100) %>%
-  image_threshold("black", "60%") %>%  # Binariza la imagen
-  image_despeckle() %>%  # Elimina el ruido
-  image_trim() %>%
-  image_ocr()
-
 # Define una función para extraer la fecha
 extract_date <- function(text) {
-  date_pattern <- "\\b\\d{2}/\\d{2}/\\d{2}\\b"  # Patrón para fechas en el formato DD/MM/YY
+  date_pattern <- "\\b\\d{2}/\\d{2}/\\d{2}\\b" # Patrón para fechas en el formato DD/MM/YY
   date <- regmatches(text, gregexpr(date_pattern, text))[[1]]
   return(date[1])
 }
 
 # Define una función para extraer la hora
 extract_time <- function(text) {
-  time_pattern <- "\\b\\d{2}:\\d{2}:\\d{2}\\b"  # Patrón para horas en el formato HH:MM:SS
+  time_pattern <- "\\b\\d{2}:\\d{2}:\\d{2}\\b" # Patrón para horas en el formato HH:MM:SS
   time <- regmatches(text, gregexpr(time_pattern, text))[[1]]
   return(time[1])
 }
 
-# Extrae la fecha y la hora
-date <- extract_date(text)
-time <- extract_time(text)
+process_image_and_extract_datetime <- function(image_path) {
+  # Verificar si el archivo existe
+  if (!file.exists(image_path)) {
+    print(paste("Not exist:", image_path))
+    return(list(date = NA, time = NA))
+  }
+  # Leer la imagen
+  image <- image_read(image_path)
 
-# Imprime la fecha y la hora
-#print(paste(text))
-print(paste("Date:", date))
-print(paste("Time:", time))
+  # Verificar si la imagen es nula
+  if (is.null(image)) {
+    print(paste("Not reading:", image))
+    return(list(date = NA, time = NA))
+  }
+
+  # width_crop <- floor(image_info(image)$width * 0.2)
+  # width_heigh <- floor(image_info(image)$height * 0.2)
+
+  # Procesar la imagen
+  processed_image <- image %>%
+    image_crop(geometry_area(x = 0, y = 0, width = 220, height = 90)) %>%
+    image_resize("200%") %>%
+    image_convert(colorspace = "gray") %>%
+    image_modulate(brightness = 120, saturation = 0, hue = 100) %>%
+    image_threshold("black", "60%") %>%
+    image_despeckle()
+
+  # Realizar el OCR
+  text <- image_ocr(processed_image, options = list(tessedit_char_whitelist = "0123456789:/"))
+
+  # Extraer la fecha y la hora
+  date <- extract_date(text)
+  time <- extract_time(text)
+
+  print(paste("Processing:", image_path, "| Date:", date, "| Time:", time))
+
+  return(list(date = date, time = time))
+}
+
+######
+# Main
+######
+
+# Obtener los nombres de las imágenes en el directorio 'frames'
+image_files <- list.files(path = "frames")
+
+# Crear un DataFrame vacío para almacenar los resultados
+df <- data.frame(frame = character(), date = character(), time = character(), stringsAsFactors = FALSE)
+
+# Iterar sobre cada archivo de imagen
+for (i in 1:length(image_files)) {
+  # Verificar si las columnas de fecha y hora ya tienen valores
+  if (is.na(df$date[i]) & is.na(df$time[i])) {
+    # Ajustar la ruta al archivo
+    image_path <- paste0("frames/", image_files[i])
+
+    result <- process_image_and_extract_datetime(image_path)
+
+    date <- ifelse(is.na(result$date), "", result$date)
+    time <- ifelse(is.na(result$time), "", result$time)
+
+    # Añadir los resultados al DataFrame
+    df <- rbind(df, data.frame(frame = image_files[i], date = date, time = time, stringsAsFactors = FALSE))
+
+    # Guardar la fila actual en el archivo CSV
+    if (i == 1) {
+      write.table(df[i, ], "updated_data.csv", sep = ",", row.names = FALSE, col.names = TRUE, na = "")
+    } else {
+      write.table(df[i, ], "updated_data.csv", sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE, na = "")
+    }
+  }
+  # Liberar memoria
+  gc()
+}
