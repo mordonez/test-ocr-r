b2ac124fbbd1797b07af360f758a3f6c390c3b61
Miguel <mordonez.sanchez@gmail.com>
2023-11-25 00:31:52 +0000

    update

--- main.r
+++ main.r
@@ -1,7 +1,7 @@
 source("utils/ocr.r") 
 
-video_path <- "video.mp4"  # Reemplaza esto con la ruta al video que quieres procesar
-csv_path <- "frames.csv"  # Reemplaza esto con la ruta al archivo CSV donde quieres guardar los datos
+video_path <- "/workspaces/test-r/video.mp4"  # Reemplaza esto con la ruta al video que quieres procesar
+csv_path <- "/workspaces/test-r/frames.csv"  # Reemplaza esto con la ruta al archivo CSV donde quieres guardar los datos
 image_dir <- "/workspaces/test-r/frames"  # Reemplaza esto con la ruta al directorio donde quieres guardar las imágenes
 
 export_frames(video_path, csv_path, image_dir)
